eb3a6343a413730eaac875753f8b6f72b2432a07
LostCarcosa <62232382+LostCarcosa@users.noreply.github.com>
2023-01-31 13:28:06 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
     <section class="section-container" id="events" style="width:435px; height:714px;">
       <div class="section-header clipped-medium-backward">
         <img src="/icons/events-icon.svg" />
-        <h1>Events Log</h1>
+        <h1 style="color: white;">Events Log</h1>
       </div>
       <div class="section-content-container">
         <Markdown :source="events" class="markdown" />
