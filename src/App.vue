0afaa131d547c0ba5c6823819fb155d5b11be848
LostCarcosa <62232382+LostCarcosa@users.noreply.github.com>
2023-02-16 15:50:04 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -114,7 +114,7 @@ export default {
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "GMS_Assault"
+          "mech": "Rigamarole"
         },
         {
           "callsign": "Flare",
@@ -122,7 +122,7 @@ export default {
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "GMS_Support"
+          "mech": "Bombs Away"
         },
         {
           "callsign": "Embers",
