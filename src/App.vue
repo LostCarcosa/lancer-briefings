b8d6cf01323a5c517dc1398852d3db9a53d6388a
LostCarcosa <62232382+LostCarcosa@users.noreply.github.com>
2023-06-25 21:02:03 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -106,7 +106,7 @@ export default {
         {
           "slug": "NRFAW001",
           "name": "Bug Hunt",
-          "status": "start"
+          "status": "success"
         }
       ],
       "pilots": [
@@ -122,9 +122,9 @@ export default {
           "callsign": "Mimosa",
           "alias": "Hypatia Doe",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Rigamarole"
+          "corpro": "SSC",
+          "frame": "Orchis",
+          "mech": "Diplomacy"
         },
         {
           "callsign": "Flare",
